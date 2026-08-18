36d65caf829de94523afbfacc76b70c3b7991c38
Julian <kallu08@hotmail.com>
2026-08-17 18:57:11 -0600

    Se agrego un tracker de las tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useReducer } from "react"
+import { useReducer, useEffect, useState } from "react"
 import Tasks from "./components/Tasks"
 import CleanTasks from "./components/CleanTasks"
 import AddTasks from "./components/AddTasks"
 import Filters from "./components/Filters"
+import Tracker from "./components/Tracker"
 import { initialState, reducer } from "./reducers/manage-reducer"
 import { loadTasks, saveTasks } from "./helpers/localStorage"
-import { useEffect } from "react"
 import { ListTodo } from "lucide-react"
-import { useState } from "react"
 
 function App() {
 
@@ -27,14 +26,19 @@ function App() {
   return (
     <>
       <main className=" bg-white md:max-w-4xl mx-auto my-12 rounded-md shadow-md">
-        <header className="mx-auto px-5 md:px-10 py-5 border-b border-zinc-200 flex gap-5">
-          <div className="bg-indigo-700 p-2 rounded-md shadow flex items-center">
-            <ListTodo size={50} color="white" />
+        <header className="mx-auto px-5 md:px-10 py-5 border-b border-zinc-200 flex flex-col md:flex-row md:justify-between gap-5">
+          <div className="flex gap-5">
+            <div className="bg-indigo-700 p-2 rounded-md shadow flex items-center">
+              <ListTodo size={50} color="white" />
+            </div>
+            <div className="">
+              <h1 className="text-2xl md:text-3xl font-bold">Gestor de Tareas</h1>
+              <p className="text-zinc-500 text-sm md:text-lg">Organiza tus tareas y sé más productivo</p>
+            </div>
           </div>
-          <div>
-            <h1 className="text-2xl md:text-3xl font-bold">Gestor de Tareas</h1>
-            <p className="text-zinc-500 text-sm md:text-lg">Organiza tus tareas y sé más productivo</p>
-          </div>
+          <Tracker
+            state={state.tasks}
+          />
         </header>
         <AddTasks
           dispatch={dispatch}
